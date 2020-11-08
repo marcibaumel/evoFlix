f7c581a6c0bf83f67347cd7398abc9fc2d3baa1e
Marthon <marcibaumel@gmail.com>
2020-11-08 23:34:19 +0100

    whoes window

--- evoFlix.WPF/DashboardWindow.xaml.cs
+++ evoFlix.WPF/DashboardWindow.xaml.cs
@@ -10,22 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace evoFlix.WPF
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for DashboardWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class DashboardWindow : Window
     {
-        public MainWindow()
+        public DashboardWindow()
         {
-            
             InitializeComponent();
         }
-
-        
     }
 }
---
+++ evoFlix.WPF/LoginWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using evoFlix.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace evoFlix.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Frame FrameRegister;
+
+        UserService userService = new UserService();
+
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+        }
+
+        public void test_button()
+        {
+            Console.WriteLine("asd");
+        }
+
+        public void creat_a_new_user()
+        {
+            test_button();
+
+        }
+
+        public void delet_a_user_button()
+        {
+
+        }
+
+        public void login_user()
+        {
+
+        }
+
+        
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            test_button();
+            FrameRegister.Visibility = Visibility.Visible;
+
+        }
+    }
+}
