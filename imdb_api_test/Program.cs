5012c0722e9be7822a944d75a949796223f71cf9
Marthon <marcibaumel@gmail.com>
2020-11-18 23:10:20 +0100

    ....

---
+++ imdb_api_test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace imdb_api_test
+{
+    class Program
+    {
+        
+        public void Main(string[] args)
+        {
+            var apiLib = new ApiLib("API-Key");
+            //var apiLib = new ApiLib("API_Key");
+            //var data = await apiLib.SearchMovieAsync("leon the professional 1994");
+        }
+
+        
+    }
+}
