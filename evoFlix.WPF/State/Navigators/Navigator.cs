aaf67e372e0f882fc03164dd4b1e102f94173c77
Marthon <marcibaumel@gmail.com>
2020-11-20 12:17:42 +0100

    OnPropertyChanged

--- evoFlix.WPF/State/Navigators/Navigator.cs
+++ evoFlix.WPF/State/Navigators/Navigator.cs
@@ -2,6 +2,7 @@
 using evoFlix.WPF.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,29 @@ using System.Windows.Input;
 
 namespace evoFlix.WPF.State.Navigators
 {
-    public class Navigator : INavigator
+    public class Navigator : INavigator, INotifyPropertyChanged
     {
-        public ViewModelBase CurrentViewModel { get; set; }
+        private ViewModelBase _currentViewModel;
+        public ViewModelBase CurrentViewModel 
+        {
+            get
+            {
+                return _currentViewModel;
+            }
+            set
+            {
+                _currentViewModel = value;
+                OnPropertyChanged(nameof(CurrentViewModel));
+            }
+        }
 
         public ICommand UpdateCurrentViewModelCommand => new UpdateCurrentViewModelCommands(this);
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
