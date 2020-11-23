6c185a6c255992617d85abeddd6c99c043853a1d
Marcell Tisza <tiszamarcell2@gmail.com>
2020-11-23 18:21:28 +0100

    Play/Pause buttons are working only. Requires local media file.

--- evoFlix.WPF/MainWindow.xaml.cs
+++ evoFlix.WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using evoFlix.WPF.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -32,7 +33,8 @@ namespace evoFlix.WPF
             */
 
             Page log = new LoginPage();
-            this.Content = log;
+            Page player = new VideoPlayer();
+            this.Content = player;
 
            
         }
---
+++ evoFlix.WPF/Views/VideoPlayer.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace evoFlix.WPF.Views
+{
+    /// TO-DO:
+    ///     - Button images
+    ///     - Update the video with slider and vica versa
+    ///     - Add tootip to the slider
+    ///     - Add logic to the other buttons
+    ///     - Go back to another page with the back button
+    ///     - Save the current time
+    ///     - Load the saved time
+    /// 
+    public partial class VideoPlayer : Page
+    {
+        public VideoPlayer()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            mdaVideo.Play();
+        }
+
+        private void btnPause_Click(object sender, RoutedEventArgs e)
+        {
+            mdaVideo.Pause();
+        }
+    }
+}
