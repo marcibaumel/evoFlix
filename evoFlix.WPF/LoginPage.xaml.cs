fd27ff271117f6fa2031a7ab9274163321e31dcc
Marthon <marcibaumel@gmail.com>
2020-12-17 12:58:34 +0100

    working user componenet

--- evoFlix.WPF/LoginPage.xaml.cs
+++ evoFlix.WPF/LoginPage.xaml.cs
@@ -37,9 +37,6 @@ namespace evoFlix.WPF
             labelControl();
             profilPitcureControl();
 
-            
-            
-
 
         }
 
@@ -56,42 +53,79 @@ namespace evoFlix.WPF
 
         public void labelControl()
         {
-            int user1_Id = ucs.listOfUsers()[1];
-           
-            if(user1_Id<0)
+            int user1_Id;
+            int user2_Id;
+            int user3_Id;
+            int user4_Id;
+
+            try
+            {
+                user1_Id = ucs.listOfUsers()[0];
+                User1_Label.Content = ucs.getUserName(user1_Id).ToString();
+
+            }
+            catch(ArgumentOutOfRangeException e)
             {
                 User1_Label.Content = "";
+                Console.WriteLine(e.Message);
             }
-            else
-            {
-                User1_Label.Content = ucs.getUserName(user1_Id).ToString();
-            }
-            
-            int user2_Id = (int)ucs.listOfUsers()[1];
-            int user3_Id = (int)ucs.listOfUsers()[1];
-            int user4_Id = (int)ucs.listOfUsers()[0];
 
-           
-            User2_Label.Content = ucs.getUserName(user2_Id).ToString();
-            User3_Label.Content = ucs.getUserName(user3_Id).ToString();
-            User4_Label.Content = ucs.getUserName(user4_Id).ToString();
+
+            try
+            {
+                user2_Id = ucs.listOfUsers()[1];
+                User2_Label.Content = ucs.getUserName(user2_Id).ToString();
+
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                User2_Label.Content = "";
+                Console.WriteLine(e.Message);
+            }
+
+
+            try
+            {
+                user3_Id = ucs.listOfUsers()[2];
+                User3_Label.Content = ucs.getUserName(user3_Id).ToString();
+
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                User3_Label.Content = "";
+                Console.WriteLine(e.Message);
+            }
+
+
+            try
+            {
+                user4_Id = ucs.listOfUsers()[3];
+                User4_Label.Content = ucs.getUserName(user4_Id).ToString();
+
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                User4_Label.Content = "";
+                Console.WriteLine(e.Message);
+            }
 
         }
         
         
         public void profilPitcureControl()
         {
-            int user1_Id = (int)ucs.listOfUsers()[1];
-            int user2_Id = (int)ucs.listOfUsers()[1];
-            int user3_Id = (int)ucs.listOfUsers()[1];
-            int user4_Id = (int)ucs.listOfUsers()[0];
+            
+           
 
-            if (user1_Id == 0 || user1_Id<0)
-            {
+            int user1_Id;
+            int user2_Id;
+            int user3_Id;
+            int user4_Id;
+            
 
-            }
-            else
+            try
             {
+                user1_Id = ucs.listOfUsers()[0];
                 Image img1 = new Image();
                 img1.Source = new BitmapImage(new Uri(@ucs.getUserProfilPitcure(user1_Id)));
 
@@ -101,44 +135,83 @@ namespace evoFlix.WPF
                 stackPnl1.Children.Add(img1);
 
                 User1_Button.Content = stackPnl1;
+
+
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                
+                Console.WriteLine(e.Message);
+                User1_Button.Content = "";
             }
 
 
-            
-            Image img2 = new Image();
-            Image img3 = new Image();
-            Image img4 = new Image();
+            try
+            {
+                user2_Id = ucs.listOfUsers()[1];
+                Image img2 = new Image();
+                img2.Source = new BitmapImage(new Uri(@ucs.getUserProfilPitcure(user2_Id)));
+
+                StackPanel stackPnl2 = new StackPanel();
+                stackPnl2.Orientation = Orientation.Horizontal;
+                stackPnl2.Margin = new Thickness(5);
+                stackPnl2.Children.Add(img2);
+
+                User2_Button.Content = stackPnl2;
 
 
-            
-            img2.Source = new BitmapImage(new Uri(@ucs.getUserProfilPitcure(user2_Id)));
-            img3.Source = new BitmapImage(new Uri(@ucs.getUserProfilPitcure(user3_Id)));
-            img4.Source = new BitmapImage(new Uri(@ucs.getUserProfilPitcure(user4_Id)));
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+
+                Console.WriteLine(e.Message);
+                User2_Button.Content = "";
+            }
 
 
-            
+            try
+            {
+                user3_Id = ucs.listOfUsers()[2];
+                Image img3 = new Image();
+                img3.Source = new BitmapImage(new Uri(@ucs.getUserProfilPitcure(user3_Id)));
 
-            StackPanel stackPnl2 = new StackPanel();
-            stackPnl2.Orientation = Orientation.Horizontal;
-            stackPnl2.Margin = new Thickness(5);
-            stackPnl2.Children.Add(img2);
+                StackPanel stackPnl3 = new StackPanel();
+                stackPnl3.Orientation = Orientation.Horizontal;
+                stackPnl3.Margin = new Thickness(5);
+                stackPnl3.Children.Add(img3);
 
-            StackPanel stackPnl3 = new StackPanel();
-            stackPnl3.Orientation = Orientation.Horizontal;
-            stackPnl3.Margin = new Thickness(5);
-            stackPnl3.Children.Add(img3);
-
-            StackPanel stackPnl4 = new StackPanel();
-            stackPnl4.Orientation = Orientation.Horizontal;
-            stackPnl4.Margin = new Thickness(5);
-            stackPnl4.Children.Add(img4);
-
-           
-            User2_Button.Content = stackPnl2;
-            User3_Button.Content = stackPnl3;
-            User4_Button.Content = stackPnl4;
+                User3_Button.Content = stackPnl3;
 
 
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+
+                Console.WriteLine(e.Message);
+                User3_Button.Content = "";
+            }
+
+            try
+            {
+                user4_Id = ucs.listOfUsers()[3];
+                Image img4 = new Image();
+                img4.Source = new BitmapImage(new Uri(@ucs.getUserProfilPitcure(user4_Id)));
+
+                StackPanel stackPnl4 = new StackPanel();
+                stackPnl4.Orientation = Orientation.Horizontal;
+                stackPnl4.Margin = new Thickness(5);
+                stackPnl4.Children.Add(img4);
+
+                User4_Button.Content = stackPnl4;
+
+
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+
+                Console.WriteLine(e.Message);
+                User4_Button.Content = "";
+            }
 
         }
 
