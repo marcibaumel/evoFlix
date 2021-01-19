eda233aaac5242242b40e7d217a89c5ae2dc0d72
Marthon <marcibaumel@gmail.com>
2021-01-19 09:00:33 +0100

    my_list update

---
+++ evoFlix.WPF/DashboardViews/MyList.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace evoFlix.WPF.DashboardViews
+{
+    /// <summary>
+    /// Interaction logic for MyList.xaml
+    /// </summary>
+    public partial class MyList : UserControl
+    {
+        public MyList()
+        {
+            InitializeComponent();
+        }
+       
+
+        private void Film_Click(object sender, RoutedEventArgs e)
+        {
+
+
+        }
+    }
+}
