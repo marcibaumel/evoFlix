2ca5f6982496ed90ffcf5ed1af0328633ced79c3
Marcell Leleszi <mleleszi@icould.com>
2021-10-26 18:48:58 +0200

    fetch all users from dummy endpoint

--- evoFlix/evoFlix/ClientApp/src/App.tsx
+++ evoFlix/evoFlix/ClientApp/src/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import FetchData from "./components/FetchData";
+
 const App = () => {
   return (
     <div>
-      <h1>hello world</h1>
+      <FetchData/>
     </div>
   );
 };
