119dfcd0a43fac6a2b9adac9bc127c35245b51bd
Baumel Márton <marcibaumel@gmail.com>
2021-12-06 18:17:19 +0100

    lorem pls

--- evoFlix/evoFlix/ClientApp/src/components/MainSite/Login/Login.tsx
+++ evoFlix/evoFlix/ClientApp/src/components/MainSite/Login/Login.tsx
@@ -4,7 +4,7 @@ function Login(){
     return(
         <>
         <div>
-            
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, quasi. Accusantium maxime illo, harum dicta ipsa officiis, aspernatur deleniti quibusdam, asperiores totam a odit architecto eius. Sunt, ipsam. Nostrum, qui!
         </div>
         </>
     )
