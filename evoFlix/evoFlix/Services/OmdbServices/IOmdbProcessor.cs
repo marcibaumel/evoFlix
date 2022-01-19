a4edb123b0702037fdd4f400d8451d720f25cf8f
Baumel Márton <marcibaumel@gmail.com>
2022-01-19 18:58:45 +0100

    omdb processor

---
+++ evoFlix/evoFlix/Services/OmdbServices/IOmdbProcessor.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace evoFlix.Services.OmdbServices
+{
+    public interface IOmdbProcessor
+    {
+        public Task<FilmDto> GetFilmByOmdbApi(string Title, string Year);
+    }
+}
---
+++ evoFlix/evoFlix/Services/OmdbServices/OmdbProcessor.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evoFlix.Services.OmdbServices
+{
+    public class OmdbProcessor : IOmdbProcessor
+    {
+
+        //TODO:
+        //-appsetting-ből olvasni az apikey-t
+
+        private string APIKEY = "f51c1d39";
+        
+
+        public async Task<FilmDto> GetFilmByOmdbApi(string Title, string Year)
+        {
+            string url = $"http://www.omdbapi.com/?apikey={APIKEY}&t={Title}&y={Year}";
+            ApiHelper.InitializeClient();
+
+
+
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync(url))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    FilmDto film = await response.Content.ReadAsAsync<FilmDto>();
+
+                    return film;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
