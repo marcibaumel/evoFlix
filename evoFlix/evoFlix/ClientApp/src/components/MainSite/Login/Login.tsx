12105c4c2a8a4cac85e8879e923e9b4b20c2efa7
Baumel Márton <marcibaumel@gmail.com>
2022-01-20 19:24:36 +0100

    working login

--- evoFlix/evoFlix/ClientApp/src/components/MainSite/Login/Login.tsx
+++ evoFlix/evoFlix/ClientApp/src/components/MainSite/Login/Login.tsx
@@ -28,20 +28,54 @@ const Login = () => {
                 setUser(data);
             });
     }, []);
+
+
+    async function loginFunction(username:string, password:string) {
+        fetch('http://localhost:8000/Users/login', {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({username: "Béla", password: "Bél@12" })
+        }).then(function(response){
+            if(response.status == 200 ||)
+        })
+        
+    }
 */
+
     const handleLogin = () =>{
         console.log(user.username, user.password);
-        alert(user.username);
+        
+        fetch('http://localhost:8000/Users/login', {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({username: user.username, password: user.password })
+        }).then(function(response){
+            if(response.status == 200 || response.status == 201){
+                alert(user.username)
+                setSuccess(true);
+            }
+            else{
+                setSuccess(false);
+                alert("Something not seems to be okay with your login data")
+            }
+        })
+        
     }
 
     return (
+        <>
+        {success ? (
+        <>
+            <img src="https://i.pinimg.com/originals/58/22/46/58224674a4868f695d1f0e4ff61bf959.gif" alt="Banner" />
+        </>
+        ):(
+        <>
         
- 
         <div className="centerCard">
             <h1 style={{ textAlign: "center", paddingBottom: "15px" }}>Login</h1>
             <form id="registrationForm">
                 <div className="input-box">
-                    <input className="form-block" type="text" name="email" value={user.username} placeholder="Username" onChange={handleChange} />
+                    <input className="form-block" type="text" name="username" value={user.username} placeholder="Username" onChange={handleChange} />
                 </div>
 
                 <div className="input-box">
@@ -49,11 +83,14 @@ const Login = () => {
                 </div>
 
                 <div className="input-box">
-                    <input type="submit" onClick={handleLogin}/>
+                    <input type="button" onClick={handleLogin} value="Login"/>
                 </div>
                 
             </form>
         </div>
+        
+        </>)}
+        </>
     );
 }
  
